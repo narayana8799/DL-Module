f73c89d1177edc7f11880bfd39c98fc9506c6c00
shivam jadhav <jadhavshivam0228@gmail.com>
2021-02-07 12:21:09 +0530

    svm added doc string

--- brain/classification/SVM.py
+++ brain/classification/SVM.py
@@ -1,5 +1,13 @@
 import numpy as np
 class Svm:
+    """
+       parameters:
+       
+       lr  : learning rate 0<=l<=1
+       
+       alfa: Lagrangian multiplier,denoted by α is a vector of the weights of
+             all the training points as support vectors.
+    """
     def __init__(self,lr=0.01,alfa=0.01,n_iters=1000):
         self.lr=lr
         self.alfa=alfa
@@ -26,59 +34,7 @@ class Svm:
         ypred=np.where(linearmod>=0,1,0)
         return ypred
 
-#running Model
 
 
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('/content/drive/MyDrive/Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
 
 
-# Fitting SVM to the Training set
-classifier = Svm()
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm= confusion_matrix(y_test, y_pred)
-tp,fp,fn,tn=cm.ravel()
-
-Ac=(tp+tn)/(tn+tp+fp+fn)
-
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-    c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
